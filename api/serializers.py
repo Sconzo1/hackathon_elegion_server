2d690b1e65679a9790404aa7b0f2da1662492499
Simraki <Simraki@mail.ru>
2021-10-02 16:48:45 +0700

    fix serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 
 from authentication.serializers import UserSerializer
-from .models import Task
+from .models import Task, ChatType, ForeignChat
 from .models import UserTask
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
+        fields = '__all__'
+
+
+class ChatTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatType
         fields = '__all__'
 
 
@@ -18,4 +24,12 @@ class UserTaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserTask
+        fields = '__all__'
+
+
+class ForeignChatSerializer(serializers.ModelSerializer):
+    type = ChatTypeSerializer(read_only=True, source='id_type')
+
+    class Meta:
+        model = ForeignChat
         fields = '__all__'
