0d9e11baf80f10220f4194e9aaac4b3d50270a4f
Simraki <Simraki@mail.ru>
2021-10-02 07:48:49 +0700

    add support of custom auth system to swagger

--- elegion/urls.py
+++ elegion/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('auth/', include('authentication.urls')),
     path('chats/', include('chats.urls')),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', include('spectacular.urls')),
+    # path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    # path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
